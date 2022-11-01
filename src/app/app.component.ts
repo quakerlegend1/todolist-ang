688117d2616c535a8bf2e490c19caf8f38b54a21
pavel_ishmukov <p-pup1@mail.ru>
2022-11-01 21:38:28 +0300

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+interface Iuser {
+  name: string
+  age: number
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'inst-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'instagram-two';
+  user: Iuser = {
+    name: 'Bobi',
+    age: 50,
+  }
+  //Property binding
+  isAppLoading = true
+
+  constructor() {
+    setTimeout(() => {
+      this.isAppLoading = false
+    }, 3000)
+  }
+
+  //Event binding
+  appTitle = 'Instagram'
+  changeTitleHandler() {
+    this.appTitle = 'New title'
+  }
+
+  //Event binding Input
+
+  text = ''
+  changeText(event: Event) {
+    this.text = (event.currentTarget as HTMLInputElement).value
+  }
+
+  newText = ''
+
+  changeNewText(event: Event) {
+    this.newText = (event.currentTarget as HTMLInputElement).value
+  }
 }
