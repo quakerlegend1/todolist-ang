877cae69eebaf249efded74af4a7e2a548741d32
pavel_ishmukov <p-pup1@mail.ru>
2022-11-02 21:50:54 +0300

    ngFpr with Object

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core'
 
+interface Lesson {
+  id: number
+  title: string
+  weekGrades: { id: number; gradeItem: number }[]
+}
+
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  grades: string[] = ['English: 9', 'Math: 4']
-  getGrade(value: string) {
-    this.grades.push(value)
-  }
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 7,
+        },
+        {
+          id: 1,
+          gradeItem: 9,
+        },
+        {
+          id: 2,
+          gradeItem: 3,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'English',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 9,
+        },
+        {
+          id: 1,
+          gradeItem: 2,
+        },
+        {
+          id: 2,
+          gradeItem: 6,
+        },
+      ],
+    },
+  ]
 }
