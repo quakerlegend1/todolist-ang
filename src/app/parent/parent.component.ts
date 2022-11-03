b78a92892a9690e5cdc22da8a099fc5bde9532d0
pavel_ishmukov <p-pup1@mail.ru>
2022-11-03 09:47:06 +0300

    homework

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core'
 
-interface Lesson {
+interface Fruit {
   id: number
-  title: string
-  weekGrades: { id: number; gradeItem: number }[]
+  name: string
+  price: number
 }
 
 @Component({
@@ -12,6 +12,15 @@ interface Lesson {
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  myString = 'I am From Rechitsa'
-  age = 25
+  fruits: Fruit[] = [
+    { id: 1, name: 'apple', price: 10 },
+    { id: 2, name: 'orange', price: 20 },
+    { id: 3, name: 'watermelon', price: 30 },
+    { id: 4, name: 'banana', price: 5 },
+    { id: 5, name: 'pears', price: 12 },
+    { id: 6, name: 'raspberries', price: 18 },
+    { id: 7, name: 'avocados', price: 14 },
+    { id: 8, name: 'mangoes', price: 3 },
+    { id: 9, name: 'kiwifruit', price: 7 },
+  ]
 }
