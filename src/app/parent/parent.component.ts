cba882eba337288e6757f65da80c7dcaddf7cdab
pavel_ishmukov <p-pup1@mail.ru>
2022-11-03 15:25:26 +0300

    pipe

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -12,15 +12,7 @@ interface Fruit {
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  fruits: Fruit[] = [
-    { id: 1, name: 'apple', price: 10 },
-    { id: 2, name: 'orange', price: 20 },
-    { id: 3, name: 'watermelon', price: 30 },
-    { id: 4, name: 'banana', price: 5 },
-    { id: 5, name: 'pears', price: 12 },
-    { id: 6, name: 'raspberries', price: 18 },
-    { id: 7, name: 'avocados', price: 14 },
-    { id: 8, name: 'mangoes', price: 3 },
-    { id: 9, name: 'kiwifruit', price: 7 },
-  ]
+  title = 'Some Title'
+  url = 'https://angular.io/api/?type=pipe'
+  date = new Date(2022, 4, 14, 10)
 }
