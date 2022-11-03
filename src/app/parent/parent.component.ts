52ea1103853802883bb54c32cda837c534f9400d
pavel_ishmukov <p-pup1@mail.ru>
2022-11-03 08:32:38 +0300

    use ngSwitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -12,49 +12,5 @@ interface Lesson {
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  isLoading = true
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 4000)
-  }
-  lessons: Lesson[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 7,
-        },
-        {
-          id: 1,
-          gradeItem: 9,
-        },
-        {
-          id: 2,
-          gradeItem: 3,
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: 'English',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 9,
-        },
-        {
-          id: 1,
-          gradeItem: 2,
-        },
-        {
-          id: 2,
-          gradeItem: 6,
-        },
-      ],
-    },
-  ]
+  value: string = ''
 }
