cd50f7a59c98b7cb932e2aa67c57d26c5c63dee2
pavel_ishmukov <p-pup1@mail.ru>
2022-11-04 11:27:47 +0300

    post and delete requests

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,6 +8,13 @@ interface Todo {
   title: string
 }
 
+interface BaseResponse<T = {}> {
+  data: T
+  messages: string[]
+  fieldErrors: string[]
+  resultCode: number
+}
+
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
@@ -15,6 +22,12 @@ interface Todo {
 })
 export class TodosComponent implements OnInit {
   todos: Todo[] = []
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
+    },
+  }
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
@@ -23,14 +36,35 @@ export class TodosComponent implements OnInit {
 
   getTodos() {
     this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.0/todo-lists', {
-        withCredentials: true,
-        headers: {
-          'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
-        },
-      })
+      .get<Todo[]>('https://social-network.samuraijs.com/api/1.0/todo-lists', this.httpOptions)
       .subscribe((res: Todo[]) => {
         this.todos = res
+      })
+  }
+
+  createTodo() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    const title = 'Angular' + randomNumber
+    this.http
+      .post<
+        BaseResponse<{
+          item: Todo
+        }>
+      >('https://social-network.samuraijs.com/api/1.0/todo-lists', { title }, this.httpOptions)
+      .subscribe(res => {
+        const newTodo = res.data.item
+        this.todos.unshift(newTodo)
+      })
+  }
+
+  deleteTodo(id: string) {
+    this.http
+      .delete<BaseResponse>(
+        `https://social-network.samuraijs.com/api/1.0/todo-lists/${id}`,
+        this.httpOptions
+      )
+      .subscribe(() => {
+        this.todos.filter(todo => todo.id !== id)
       })
   }
 }
