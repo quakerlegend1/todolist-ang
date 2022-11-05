b7e34b3e46c9af3f88109ffaedb7ad5bc63dd0ee
pavel_ishmukov <p-pup1@mail.ru>
2022-11-05 17:21:46 +0300

    page not found

--- src/app/app-routing-routing.module.ts
+++ src/app/app-routing-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from 'src/app/components/login/login.component'
 import { TodosComponent } from 'src/app/components/todos/todos.component'
 import { UsersComponent } from 'src/app/components/users/users.component'
 import { ProfileComponent } from 'src/app/components/profile/profile.component'
+import { PageNotFoundComponent } from 'src/app/components/page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'todos', component: TodosComponent },
   { path: 'users', component: UsersComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '404' },
 ]
 
 @NgModule({
