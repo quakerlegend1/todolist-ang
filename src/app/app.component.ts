e946fe9b7c65911d4a838970b107ded39642b8ce
pavel_ishmukov <p-pup1@mail.ru>
2022-11-05 16:19:37 +0300

    navigate and active link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,10 @@
 import { Component } from '@angular/core'
 
-interface Iuser {
-  name: string
-  age: number
-}
-
 @Component({
   selector: 'inst-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  user: Iuser = {
-    name: 'Bobi',
-    age: 50,
-  }
-  //Property binding
-  isAppLoading = true
-
-  constructor() {
-    setTimeout(() => {
-      this.isAppLoading = false
-    }, 3000)
-  }
-
-  //Event binding
-  appTitle = 'Instagram'
-  changeTitleHandler() {
-    this.appTitle = 'New title'
-  }
-
-  //Event binding Input
-
-  text = ''
-  changeText(event: Event) {
-    this.text = (event.currentTarget as HTMLInputElement).value
-  }
-
-  newText = ''
-
-  changeNewText(event: Event) {
-    this.newText = (event.currentTarget as HTMLInputElement).value
-  }
-
-  //Two-way binding
-
-  city = 'London'
+  constructor() {}
 }
