d48765af1cc8930bc8713f6387966d9a51d03cdb
pavel_ishmukov <p-pup1@mail.ru>
2022-11-12 11:09:01 +0300

    filters and footer in todo

---
+++ src/app/todos/components/todos/todo/todo-filters/todo-filters.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'tl-todo-filters',
+  templateUrl: './todo-filters.component.html',
+  styleUrls: ['./todo-filters.component.css'],
+})
+export class TodoFiltersComponent {}
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -6,9 +6,18 @@ import { TodoComponent } from './components/todos/todo/todo.component'
 import { FormsModule } from '@angular/forms'
 import { TasksComponent } from './components/todos/todo/tasks/tasks.component'
 import { TaskComponent } from './components/todos/todo/tasks/task/task.component'
+import { TodoFiltersComponent } from './components/todos/todo/todo-filters/todo-filters.component'
+import { TodoFooterComponent } from './components/todos/todo/todo-footer/todo-footer.component'
 
 @NgModule({
-  declarations: [TodosComponent, TodoComponent, TasksComponent, TaskComponent],
+  declarations: [
+    TodosComponent,
+    TodoComponent,
+    TasksComponent,
+    TaskComponent,
+    TodoFiltersComponent,
+    TodoFooterComponent,
+  ],
   imports: [CommonModule, TodosRoutingModule, FormsModule],
 })
 export class TodosModule {}
