c988d7143f656badb351da5a5d77b2034324e0ea
pavel_ishmukov <p-pup1@mail.ru>
2022-11-14 12:11:11 +0300

    Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./todos/todos.module').then(el => el.TodosModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(el => el.AuthModule),
+  },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,13 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 
 import { AppRoutingModule } from 'src/app/app-routing.module'
-import { TodosModule } from 'src/app/todos/todos.module'
-import { AuthModule } from 'src/app/auth/auth.module'
 import { CoreModule } from 'src/app/core/core.module'
 import { HttpClientModule } from '@angular/common/http'
 import { SharedModule } from 'src/app/shared/shared.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TodosModule,
-    AuthModule,
-    CoreModule,
-    HttpClientModule,
-    SharedModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
